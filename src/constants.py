92f154564300a9c2885f15fb19f70422e893a6c1
hasnalathifah <91044599+hasnalathifah@users.noreply.github.com>
2023-06-12 16:20:28 +0700

    Add files via upload

---
+++ src/constants.py
@@ -0,0 +1,34 @@
+"""
+Constants for Blokus
+"""
+
+# enable debugging (print to console)
+
+ENABLE_INTRO = True
+
+# colors that are used
+COLORS = {
+    "BLACK": [0, 0, 0],
+    "WHITE": [255, 255, 255],
+    "RED": [255, 0, 0],
+    "GREEN": [0, 255, 0],
+    "BLUE": [0, 0, 255],
+    "PURPLE": [128, 0, 128],
+    "ORANGE": [255, 128, 0],
+    "NAVY": [0, 128, 128],
+}
+
+# window properties
+CLIENT_CAPTION = "Blokus Client"
+SERVER_CAPTION = "Blokus Server"
+WINDOW_ICON = "D:/progjar/BlockBattle/static/assets/img/blockbattle-icon.png"
+INTRO_LOGO = "D:/progjar/BlockBattle/static/assets/img/blockbattle.png"
+
+# game window specification
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+WINDOW_SIZE = [WINDOW_WIDTH, WINDOW_HEIGHT]
+
+
+
+
---
+++ src/server.py
@@ -0,0 +1,73 @@
+import socket  # for networking
+import sys
+from threading import Thread  # for threading
+
+# HOST = '192.168.100.193'
+HOST = socket.gethostbyname(socket.gethostname())  # this address is the ipv4
+PORT = 8080  # port to listen on for clients
+
+# set up the server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind((HOST, PORT))
+s.listen(2)
+
+# print
+print(f"-------------------------------------------\n"
+      f"Input client's server connection.\n"
+      f"Server IP   :{HOST}\n"
+      f"Server PORT :{PORT}\n"
+      f"-------------------------------------------")
+
+# set up the client
+list_of_clients = []
+
+
+def client_thread(conn):
+    while True:
+        try:
+            data = conn.recv(1024)
+            broadcast(data, conn)
+        except:
+            continue
+
+
+def broadcast(data, connection):
+    for client in list_of_clients:
+        if client != connection:
+            try:
+                client.send(data)
+            except:
+                client.close()
+                remove(client)
+
+
+def remove(connection):
+    if connection in list_of_clients:
+        list_of_clients.remove(connection)
+
+
+# accept a connection from the client
+try:
+    while True:
+        client_socket, client_address = s.accept()
+
+        for client in list_of_clients:
+            client.close()
+        list_of_clients = [client_socket]
+
+        client_socket.send('p1'.encode())
+        Thread(target=client_thread, args=(client_socket,)).start()
+        print(f"\nConnected to {client_address}!")
+
+        client_socket, client_address = s.accept()
+        list_of_clients.append(client_socket)
+        client_socket.send('p2'.encode())
+        Thread(target=client_thread, args=(client_socket,)).start()
+        print(f"\nConnected to {client_address}!")
+
+        print("\nGAME START !!!")
+
+except KeyboardInterrupt:
+    s.close()
+    sys.exit(0)
